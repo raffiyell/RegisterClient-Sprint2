f243b476d2d64af90d5d6af5839be17801930ad2
Gabriel <gddelcar@uark.edu>
2019-04-04 23:24:39 -0500

    Added confirmation screen activity

---
+++ app/src/main/java/edu/uark/uarkregisterapp/ConfirmationScreenActivity.java
@@ -0,0 +1,13 @@
+package edu.uark.uarkregisterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmationScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_screen);
+    }
+}
