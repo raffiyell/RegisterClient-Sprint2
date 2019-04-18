ab992cc0d9ada1454f1652d9603e20686794678a
raffiyell <46834718+raffiyell@users.noreply.github.com>
2019-04-18 14:27:15 -0500

    fix some stuff for search

--- app/src/main/java/edu/uark/uarkregisterapp/models/api/enums/ProductApiMethod.java
+++ app/src/main/java/edu/uark/uarkregisterapp/models/api/enums/ProductApiMethod.java
@@ -7,7 +7,7 @@ import edu.uark.uarkregisterapp.models.api.interfaces.PathElementInterface;
 
 public enum ProductApiMethod implements PathElementInterface {
 	NONE(""),
-	BY_LOOKUP_CODE("bylookupcode");
+	BY_LOOKUP_CODE("byLookupCode");
 
 	@Override
 	public String getPathValue() {
